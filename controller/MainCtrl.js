db50b2dd59602576d5abf3351ba7779debb94a41
jasondawson <jason.c.dawson@gmail.com>
2015-02-09 10:28:04 -0700

    added notification when posting updates

--- controller/MainCtrl.js
+++ controller/MainCtrl.js
@@ -1,13 +1,16 @@
 (function() {
+
 angular
 	.module('personalApi')
 	.controller('MainCtrl', MainCtrl);
 
-function MainCtrl(mainService, $location) {
+function MainCtrl(mainService, $location, $timeout) {
 	var vm = this;
 	vm.user = {};
 	vm.location = '';
 	vm.mention = '';
+	vm.locUpdated = false;
+	vm.mentionUpdated = false;
 
 	vm.getUser = function(updated) {
 		mainService.getUser(updated)
@@ -30,12 +33,22 @@ function MainCtrl(mainService, $location) {
 	vm.updateLocation = function() {
 		mainService.updateLocation(vm.location).then(function() {
 			vm.getUser(true);
+			vm.location = '';
+			vm.locUpdated = true;
+			$timeout(function(){
+				vm.locUpdated = false;
+			}, 3000);
 		});
 	}
 
 	vm.addMention = function() {
 		mainService.addMention(vm.mention).then(function() {
 			vm.getUser(true);
+			vm.mention = '';
+			vm.mentionUpdated = true;
+			$timeout(function(){
+				vm.mentionUpdated = false;
+			}, 3000);
 		})
 	}
 
